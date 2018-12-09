16a63b736cac4bc68f3b0555f5280b6aac1efd02
xufaj <45463607+xufaj@users.noreply.github.com>
2018-12-09 17:20:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "d";
 var adminprefix = '²'
-const developers = ["الايدي"]
+const developers = ["408518562521546764"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
